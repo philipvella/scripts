876d4ade506048b46e8e185593dadfac89a538fc
philipvella <philip.v.vella@gmail.com>
2024-02-27 14:32:45 +0000

    adding find my commit message

--- git/find_cmmits_by_prefix.sh
+++ git/find_cmmits_by_prefix.sh
@@ -10,11 +10,8 @@ PATTERN=$1
 
 cd ~/work/kingmakers-frontend/
 
-# Use git log to extract commits, then grep for the pattern and following lines,
-# and finally filter lines starting with "- [x]"
-git log --grep="$PATTERN" --pretty=format:"%B" | grep -E "^\- \[x\]"
-
-# Explanation of commands:
-# - `git log --grep="$PATTERN" --pretty=format:"%B"` gets the full commit messages that contain the pattern.
-# - `grep -E "^\- \[x\]"` filters those lines starting with "- [x]".
-
+# Extract tasks, remove duplicates while preserving order, then reverse the output using tail -r for macOS.
+git log --grep="$PATTERN" --pretty=format:"%B" |
+grep -E "^\- \[x\]" |
+awk '!seen[$0]++' |
+tail -r
